1d64b7c382cd084b919409c51c60ad46d4b9db45
SwiftyKey <slipperyfish9@gmail.com>
2024-05-10 21:34:36 +0500

    добавил изменение темы

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,24 +1,27 @@
-﻿using System.Text;
+﻿using ModernWpf;
+using System.Diagnostics;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace News
 {
-	/// <summary>
-	/// Interaction logic for MainWindow.xaml
-	/// </summary>
-	public partial class MainWindow : Window
+	public partial class MainWindow
 	{
 		public MainWindow()
 		{
 			InitializeComponent();
+		}
+
+		private void ToggleTheme(object sender, RoutedEventArgs e)
+		{
+			if (ThemeManager.Current.ActualApplicationTheme == ApplicationTheme.Dark)
+				ThemeManager.Current.ApplicationTheme = ApplicationTheme.Light;
+			else
+				ThemeManager.Current.ApplicationTheme = ApplicationTheme.Dark;
+		}
+
+		private void Window_ActualThemeChanged(object sender, RoutedEventArgs e)
+		{
+			Debug.WriteLine(ThemeManager.GetActualTheme(this));
 		}
 	}
 }
--- Utilities/RelayCommand.cs
+++ Utilities/RelayCommand.cs
@@ -2,21 +2,15 @@
 
 namespace News.Utilities
 {
-	class RelayCommand : ICommand
+	class RelayCommand(Action<object?> execute, Func<object?, bool>? canExecute = null) : ICommand
 	{
-		private readonly Action<object?> execute;
-		private readonly Func<object?, bool>? canExecute;
+		private readonly Action<object?> execute = execute;
+		private readonly Func<object?, bool>? canExecute = canExecute;
 
 		public event EventHandler? CanExecuteChanged
 		{
 			add { CommandManager.RequerySuggested += value; }
 			remove { CommandManager.RequerySuggested -= value; }
-		}
-
-		public RelayCommand(Action<object?> execute, Func<object?, bool>? canExecute = null)
-		{
-			this.execute = execute;
-			this.canExecute = canExecute;
 		}
 
 		public bool CanExecute(object? parameter) => canExecute == null || canExecute(parameter);
